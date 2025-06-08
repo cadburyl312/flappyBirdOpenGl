10101bfd33e8805db6727d3c60597b10a2a9c216
cadburyl312 <bremmer567@gmail.com>
2025-06-08 17:35:33 +0100

    modify main

--- source/main.cpp
+++ source/main.cpp
@@ -9,5 +9,6 @@ int main()
     std::cout << "Hello World!\n";
     Bird firstBird = Bird(5, 5, 5, bird1);
     std::cout << std::fixed << std::setprecision(2) << firstBird.getBirdX() << '\n';  
+    std::cout << std::fixed << std::setprecision(2) << firstBird.getBirdY() << '\n';  
     
 }
