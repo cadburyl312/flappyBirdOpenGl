fd860c8bc76870bd4c5aaca8f05f011cf1f72cf8
cadburyl312 <bremmer567@gmail.com>
2025-06-08 21:27:44 +0100

    move away from opengl imort and add gl debug output

--- source/main.cpp
+++ source/main.cpp
@@ -2,10 +2,11 @@
 #include <iomanip>
 #include <format>
 #include "bird.h"
+#include <GL/glew.h>
 #include <GL/freeglut.h>
-#include <GL/gl.h>
 
 void display();
+void ResizeFunction(int width, int height);
 
 int main(int argc, char** argv)
 {
@@ -19,7 +20,12 @@ int main(int argc, char** argv)
               << firstBird.getBirdZ() << '\n';
 
     glutInit(&argc, argv);
+    glutInitContextVersion(4, 0);
+
+    glutInitContextFlags(GLUT_FORWARD_COMPATIBLE);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
+
     glutInitWindowSize(800, 600);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("My First FreeGLUT Window");
@@ -27,7 +33,14 @@ int main(int argc, char** argv)
     // Set your clear color here (e.g. teal)
     glClearColor(0.0f, 0.5f, 0.5f, 1.0f);
 
+    glutReshapeFunc(ResizeFunction);
     glutDisplayFunc(display);
+    fprintf(
+        stdout,
+        "INFO: OpenGL Version: %s\n",
+        glGetString(GL_VERSION)
+    );
+
     glutMainLoop();
     return 0;
 }
@@ -36,4 +49,9 @@ void display() {
     // Clear the color buffer to the color set by glClearColor
     glClear(GL_COLOR_BUFFER_BIT);
     glFlush();
+}
+
+void ResizeFunction(int Width, int Height)
+{
+  glViewport(0, 0, Width, Height);
 }
